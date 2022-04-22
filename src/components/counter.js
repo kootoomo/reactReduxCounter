ff301f12f1e23393242dc4a593976eaaa9049f7f
koo <kootoomo@gmail.com>
2022-04-22 16:24:25 +0300

    add title, flower, colours

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {connect} from 'react-redux';
 import * as actions from '../actions';
 // import {bindActionCreators} from 'redux';
-import { Button } from 'reactstrap';
+import { Button, Badge } from 'reactstrap';
 
 const Counter = ({counter, inc, dec, rnd}) => {
     return(
         <div className="jumbotron">
-            <h1>{counter}</h1>
+            <Badge color="danger"><h1>{counter}</h1></Badge>
+            <br></br>
+            <br></br>
             <Button onClick={dec} color="primary" className="col-3">DEC</Button>
             <Button onClick={inc} color="success" className="col-3">INC</Button>
             <Button onClick={rnd} color="warning" className="col-3">RND</Button>
